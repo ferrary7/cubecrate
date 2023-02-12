66c2563e8867c7dda18e9d2dc188d08d8f5f025f
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-02-12 22:46:04 +0530

    test

--- src/components/ProjectsPage/Projects.js
+++ src/components/ProjectsPage/Projects.js
@@ -1,5 +1,19 @@
 import React from "react";
+import "./Projects.css";
 
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <>
+      <div className="projects-hero">
+        <h1 className="heading">Our Work</h1>
+        <div className="services-menu">
+          <div className="menu-item">Web Development</div>
+          <div className="menu-item">UI Design</div>
+          <div className="menu-item active">Logo Design</div>
+          <div className="menu-item">Video Editing</div>
+          <div className="menu-item">Thumbnail Design</div>
+        </div>
+      </div>
+    </>
+  );
 }
